fd25c2800b555836972729c971a143cac33dd7f1
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-06-29 17:51:24 +0100

    wip@ adding footer script partial

--- build-scripts/copy-assets.js
+++ build-scripts/copy-assets.js
@@ -1,14 +1,17 @@
 const fsExtra = require("fs-extra");
 const path = require("path");
 
+const jsPathFragment = 'js/';
 const cssPathFragment = 'css/';
-const bulmaCss = 'css/bulma.css';
-const bulmaCssSourcePath = `../node_modules/bulma/${bulmaCss}`;
+const nodeLibsPath = "../node_modules/";
 const targetPath = '../public/';
+
+const bulmaCss = `${cssPathFragment}bulma.css`;
+const bulmaCssSourcePath = `${nodeLibsPath}bulma/${bulmaCss}`;
 
 const sourceDir = path.join(__dirname, bulmaCssSourcePath);
 
-console.log("Removing existing css assets in build folder");
+console.log("Removing existing css assets in public folder");
 
 fsExtra.removeSync(path.join(__dirname, `${targetPath}${cssPathFragment}`));
 
@@ -21,6 +24,17 @@ console.log("Attempting to copying css assets to build folder");
 });*/
 
 fsExtra.copySync(sourceDir, path.join(__dirname, `${targetPath}${bulmaCss}`));
+
+console.log("Removing existing js assets in public folder");
+
+fsExtra.removeSync(path.join(__dirname, `${targetPath}${jsPathFragment}`));
+
+const createJSLibFile = "createjs.js";
+const createJSSourcePath = path.join(__dirname, `${nodeLibsPath}createjs/builds/1.0.0/${createJSLibFile}`);
+
+console.log("Attempting to copying js assets to build folder");
+
+fsExtra.copySync(createJSSourcePath, path.join(__dirname, `${targetPath}${jsPathFragment}${createJSLibFile}`));
 
 console.log("Copying assets complete!");
 
