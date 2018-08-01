2a75a2b8f0fc20b0c5b1e219957c7fc3ecaf0be6
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-08-01 15:47:53 +0100

    wip: experiment 5

---
+++ assets/ts/exp/exp5.ts
@@ -0,0 +1,78 @@
+///<reference path="../defs.d.ts"/>
+///<reference path="../steps-lib.ts"/>
+///<reference path="../steps/thank-you.ts"/>
+///<reference path="../steps/play-ar.ts"/>
+///<reference path="../steps/ar-game.ts"/>
+///<reference path="../steps/sign-up.ts"/>
+///<reference path="../steps/well-done.ts"/>
+///<reference path="../steps/amazing.ts"/>
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    const sunburstAnimateClassname: string = "animated";
+
+    const stepsHandler: StepsLib = new StepsLib();
+    const backgroundController: BGController = new BGController();
+    const thankyouController: ThankYou = new ThankYou();
+    const playARController: PlayAR = new PlayAR();
+    const arGameController: ARGame = new ARGame();
+    const wellDoneController: WellDone = new WellDone();
+    const haveCoinController: HaveFaveCoin = new HaveFaveCoin();
+    const amazingController: Amazing = new Amazing();
+
+    backgroundController.animateBG();
+
+    const secondStep: Promise<Element> = thankyouController.animateThankYouStep().then(() => {
+        playARController.bounceButton();
+        return <Promise<Element>>stepsHandler.animateToNextStep();
+    });
+
+    secondStep.then(() => {
+        playARController.doIntroAnimation();
+    });
+
+    const thirdStep: Promise<Element> = secondStep.then(() => { return playARController.createPlayButtonBinding(); }).then(() => {
+        backgroundController.animateBG(false);
+        return <Promise<Element>>stepsHandler.animateToNextStep(2000);
+    });
+
+    const fourthStep: Promise<void> = thirdStep.then(() => {
+        arGameController.startARGame();
+        arGameController.endARGame();
+    });
+
+    const fourthStepPause: Promise<void> = fourthStep.then(() => {
+        return ARGame.createDelayPromise((resolve: PromiseResolver<void>) => {
+            resolve();
+        }, 1000);
+    });
+
+    const fifthStep: Promise<Element> = fourthStepPause.then(() => {
+        arGameController.hideStep();
+        backgroundController.animateBG(true);
+        return stepsHandler.animateToNextStep();
+    });
+
+    const sixthStep: Promise<void> = fifthStep.then(() => {
+        return wellDoneController.animateWellDoneStep();
+    }).then(() => {
+        return stepsHandler.animateToNextStep();
+    }).then(() => {
+        backgroundController.animateBG(false);
+    });
+
+    const signUpSubmitPromise: Promise<void> = sixthStep.then(() => {
+        return haveCoinController.createClickBinding();
+    });
+
+    const seventhStep: Promise<Element> = signUpSubmitPromise.then(() => {
+        haveCoinController.doExitAnimation();
+        amazingController.doIntroAnimation();
+        return <Promise<Element>>stepsHandler.animateToNextStep();
+    });
+
+    seventhStep.then(() => {
+        amazingController.doExitAnimation();
+    });
+});
