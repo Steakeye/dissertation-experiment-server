ea7221b9f88bb9e833538c973b8d0160cc52edaa
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-08-05 19:44:41 +0100

    getting access to the bottles in the ar-game step

--- assets/ts/steps/ar-game.ts
+++ assets/ts/steps/ar-game.ts
@@ -1,6 +1,14 @@
 class ARGame extends BaseStep {
     constructor() {
         super('#ar-game');
+
+        this.scene = <AFrame.Scene>Sizzle("a-scene", this.stepContainer)[0];
+
+        this.sceneBottles = <AFrame.Entity[]>Sizzle(".bottle", this.scene);
+
+        this.bindCallbacks();
+
+        this.setupLoadedHandler();
     }
 
     public doIntroAnimation() {
@@ -18,4 +26,24 @@ class ARGame extends BaseStep {
     public endARGame() {
         console.log('Ending AR Game');
     }
+
+    private setupLoadedHandler() {
+        const scene: AFrame.Scene = <AFrame.Scene>this.scene;
+        if (scene.hasLoaded) {
+            this.onSceneLoaded();
+        } else {
+            scene.addEventListener("loaded", this.onSceneLoaded)
+        }
+    }
+
+    private bindCallbacks() {
+        this.onSceneLoaded = this.onSceneLoaded.bind(this);
+    }
+
+    private onSceneLoaded() {
+        console.log("a-frame scene loaded!")
+    }
+
+    protected scene?: AFrame.Scene;
+    protected sceneBottles?: AFrame.Entity[];
 }
